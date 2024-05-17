154f2a4ad9d35559aba8ede98456a9612c8f5f53
Mubarak <enpisb251@gmail.com>
2024-05-17 13:06:24 +0530

    Initial Commit for code-along codelab

--- app/src/main/java/com/example/composeexample/MainActivity.kt
+++ app/src/main/java/com/example/composeexample/MainActivity.kt
@@ -1,38 +1,11 @@
 package com.example.composeexample
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.composeexample.data.SampleData
 import com.example.composeexample.ui.theme.ComposeExampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,101 +16,10 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     Modifier.fillMaxSize()
                 ) {
-                    // MessageCard(message = Message("Mbyt", "developer"))
-                    Conversation(message = SampleData.conversationSample)
+
                 }
             }
         }
     }
 }
 
-data class Message(val author: String, val body: String)
-
-@Composable
-fun MessageCard(message: Message) {
-
-    Row(Modifier.padding(all = 8.dp)) {
-        Image(
-            painter = painterResource(id = R.drawable.nature_pic),
-            contentDescription = "userImage",
-            modifier = Modifier
-                .size(60.dp)
-                .clip(CircleShape)
-                .border(
-                    1.5.dp,
-                    MaterialTheme.colorScheme.inversePrimary,
-                    MaterialTheme.shapes.medium
-                ),
-            contentScale = ContentScale.FillWidth,
-
-            )
-
-        var isExpanded by remember { mutableStateOf(false) }
-        // surfaceColor will be updated gradually from one color to the other
-        val surfaceColor by animateColorAsState(
-            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
-        )
-
-        Spacer(modifier = Modifier.size(12.dp))
-
-        Column(Modifier.clickable { isExpanded = !isExpanded }){ // column place the child item in horizontal
-            Text(
-                text = message.author,
-                color = MaterialTheme.colorScheme.primary,
-                style = MaterialTheme.typography.titleSmall
-            )
-
-            Surface(
-                shape = MaterialTheme.shapes.medium,
-                shadowElevation = 1.dp,
-                // surfaceColor color will be changing gradually from primary to surface
-                color = surfaceColor,
-                // animateContentSize will change the Surface size gradually
-                modifier = Modifier
-                    .animateContentSize()
-                    .padding(1.dp)
-            ) {
-                Text(
-                    text = message.body,
-                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-                    modifier = Modifier.padding(4.dp),
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            }
-
-        }
-    }
-}
-
-@Preview("Light Mode")
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Night mode"
-)
-@Composable
-fun PrevCard() {
-    ComposeExampleTheme {
-        Surface {
-            MessageCard(message = Message("Mb", "Developer"))
-        }
-
-    }
-}
-
-@Composable
-fun Conversation(message: List<Message>) {
-    LazyColumn {
-        items(message) {
-            MessageCard(message = it)
-        }
-    }
-}
-
-@Preview
-@Composable
-fun PreviewConversation() {
-    Surface {
-        Conversation(message = SampleData.conversationSample)
-    }
-}
